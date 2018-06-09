942ee939e2e6b2b14e772fd34146e88e61f465b2
jesushilarioh <jesushilariohernandez@gmail.com>
2018-06-09 11:52:00 -0500

    addded to style.css and read.php

--- PHP/app-0001-taniarascia/public/read.php
+++ PHP/app-0001-taniarascia/public/read.php
@@ -1,4 +1,70 @@
+<?php
+    if (isset($_POST['submit'])) {
+        try {
+            require "../config.php";
+            require "../common.php";
+
+            $connection = new PDO($dsn, $username, $password, $options);
+
+            // fetch data code will go here
+            // Select * from users by location
+            $sql = "SELECT * FROM users
+                    WHERE location = :location";
+
+            // Put $_POST into a variable
+            $location = $_POST["location"];
+
+            // Prepare, bind, and execute ($statement)
+            // Initialize $statement
+            $statement = $connection -> prepare($sql);
+            $statement -> bindParam(":location", $location, PDO::PARAM_STR);
+            $statement -> execute();
+
+            // fetch the ($statement) result into $result
+            $result = $statement -> fetchAll();
+
+        } catch (PDOException $error) {
+            echo $sql. "<br>". $error -> getMessage();
+        }
+    }
+
+?>
 <?php include "templates/header.php"; ?>
+<?php
+    // Check if more that 0 rows exist
+    if (isset($_POST["submit"])) {
+        if ($result && $statement -> rowCount() > 0) { ?>
+            <h2>Results</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Location</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+        <?php foreach ($result as $row){ ?>
+                <tr>
+                    <td><?php echo escape($row["id"]); ?></td>
+                    <td><?php echo escape($row["firstname"]); ?></td>
+                    <td><?php echo escape($row["lastname"]); ?></td>
+                    <td><?php echo escape($row["email"]); ?></td>
+                    <td><?php echo escape($row["age"]); ?></td>
+                    <td><?php echo escape($row["location"]); ?></td>
+                    <td><?php echo escape($row["date"]); ?></td>
+                </tr>
+        <?php } ?>
+                </tbody>
+            </table>
+    <?php } else { ?>
+            <blockquote>No results found for <?php echo escape($_POST["location"]); ?>.</blockquote>
+    <?php }
+    } ?>
 
 <h2>Find user based on location</h2>
 
